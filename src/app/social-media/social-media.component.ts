624da908eb6faaf488da281a1cfa7a3b355884ac
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-09-19 22:34:29 +0200

    extend social media links with some properties

--- src/app/social-media/social-media.component.ts
+++ src/app/social-media/social-media.component.ts
@@ -7,6 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SocialMediaComponent implements OnInit {
 
+  @Input() light: boolean = true;
+  @Input() big: boolean = true;
+  @Input() center: boolean = false;
+
   constructor() { }
 
   ngOnInit() { }
